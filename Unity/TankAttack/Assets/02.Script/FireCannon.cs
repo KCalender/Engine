b1260078dd8cd523dbab1203a291922d0e77bcff
Calender <pdt0616@gmail.com>
2021-04-14 22:21:50 +0900

    2021-04-15
    절대강좌 4/15
    14챕터 네트워킹 시작

---
+++ Unity/TankAttack/Assets/02.Script/FireCannon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCannon : MonoBehaviour
+{
+    //cannon 프리팹을 연결할 변수
+    public GameObject cannon = null;
+    //포탄 발사 사운드 파일
+    public AudioClip fireSfx = null;
+    //AudioSource 컴포넌트를 할당할 변수
+    private AudioSource sfx = null;
+    //cannon 발사 지점
+    public Transform firePos;
+
+    private void Awake()
+    {
+        //cannon 프리팹을 Resources 폴더에서 불러와 변수에 할당
+        cannon = (GameObject)Resources.Load("Cannon");
+        //포탄 발사 사운드 파일을 Resources 폴더에서 불러와 변수에 할당
+        fireSfx = (AudioClip)Resources.Load("CannonFire");
+        //AudioSource 컴포넌트 할당
+        sfx = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        sfx.PlayOneShot(fireSfx, 1.0f);
+        Instantiate(cannon, firePos.position, firePos.rotation);
+    }
+}
---
+++ Unity/TankAttack/Assets/02.Script/TurretCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretCtrl : MonoBehaviour
+{
+    private Transform tr;
+    //ray가 지면에 맞은 위치 저장
+    private RaycastHit hit;
+
+    //터렛의 회전 속도
+    public float rotSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //메인 카메라에서 마우스 커서의 위치로 캐스팅되는 Ray 생성
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //생성된 Ray 를 Scene 뷰에ㅔ 녹색 광선으로 표현
+        Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, 1<<8))
+        {
+            //Ray에 맞은 위치를 로컬 좌표로 변환
+            Vector3 relative = tr.InverseTransformPoint(hit.point);
+            //역 탄젠트 함수인 Atan2 로 두 점 간의 각도를 계산
+            float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+            //rotSpeed 변수에 지정된 속도로 회전
+            tr.Rotate(0, angle * Time.deltaTime * rotSpeed, 0);
+        }
+    }
+}
