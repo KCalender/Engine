e5a916f1f40a1da163745f9780f9c7de01290d12
Calender <pdt0616@gmail.com>
2021-04-16 22:37:16 +0900

    2021-04-16

---
+++ Unity/TankAttack/Assets/02.Script/DisplayUserId.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DisplayUserId : MonoBehaviour
+{
+    public Text userId;
+    private PhotonView pv = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+        userId.text = pv.owner.NickName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Unity/TankAttack/Assets/02.Script/TankDamage.cs
+++ Unity/TankAttack/Assets/02.Script/TankDamage.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankDamage : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class TankDamage : MonoBehaviour
     private int initHp = 100;
     private int currHp = 0;
 
+    //UI 연결용
+    public Canvas hudCanvas;
+    public Image hpBar;
+
     private void Awake()
     {
         //탱크 모델의 모든 MeshRenderer 컴포넌트를 추출한 후 배열에 할당
@@ -23,6 +28,8 @@ public class TankDamage : MonoBehaviour
 
         //탱크 폭발 시 생성시킬 폭발 효과를 로드
         expEffect = Resources.Load<GameObject>("Large Explosion");
+
+        hpBar.color = Color.green;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,6 +38,14 @@ public class TankDamage : MonoBehaviour
         if (currHp > 0 && other.tag == "CANNON")
         {
             currHp -= 20;
+
+            hpBar.fillAmount = (float)currHp / (float)initHp;
+
+            if (hpBar.fillAmount <= 0.4f)
+                hpBar.color = Color.red;
+            else if (hpBar.fillAmount <= 0.6f)
+                hpBar.color = Color.yellow;
+
             if(currHp <= 0)
             {
                 StartCoroutine(this.ExplosionTank());
@@ -46,11 +61,18 @@ public class TankDamage : MonoBehaviour
 
         Destroy(effect, 3.0f);
 
+        //hud 비활성화
+        hudCanvas.enabled = false;
+        
         //탱크 투명 처리
         SetTankVisible(false);
         //3초 기다렸다가 활성화
         yield return new WaitForSeconds(3.0f);
 
+        //hud 초기화
+        hpBar.fillAmount = 1.0f;
+        hpBar.color = Color.green;
+        hudCanvas.enabled = true;
 
         currHp = initHp;
         SetTankVisible(true);
