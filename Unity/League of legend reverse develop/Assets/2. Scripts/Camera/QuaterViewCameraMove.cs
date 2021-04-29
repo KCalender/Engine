bf860c09dd545e611e0c9ec9ca344e5e1764e818
Calender <pdt0616@gmail.com>
2021-04-29 22:22:42 +0900

    롤 역개발
    카메라 이동 구현

---
+++ Unity/League of legend reverse develop/Assets/2. Scripts/Camera/QuaterViewCameraMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaterViewCameraMove : MonoBehaviour
+{
+    public Transform Target;
+    public bool isFocus = false;
+
+    private float h, v;
+
+    private int LeftS, RightS, TopS, BottomS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeftS = Screen.width / 30; 
+        RightS = Screen.width - LeftS;
+        BottomS = Screen.height / 30;
+        TopS = Screen.height - BottomS;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFocus)
+        {
+            Vector3 moveDir = new Vector3();
+
+            Vector3 MousePoint = Input.mousePosition;
+            if (MousePoint.x < LeftS && MousePoint.y > TopS)
+                moveDir = Vector3.forward + Vector3.left;
+            else if (MousePoint.x > RightS && MousePoint.y > TopS)
+                moveDir = Vector3.forward + Vector3.right;
+            else if (MousePoint.x < LeftS && MousePoint.y < BottomS)
+                moveDir = Vector3.back + Vector3.left;
+            else if (MousePoint.x > RightS && MousePoint.y > TopS)
+                moveDir = Vector3.back + Vector3.right;
+            else if (MousePoint.x > RightS)
+                moveDir = Vector3.right;
+            else if (MousePoint.x < LeftS)
+                moveDir = Vector3.left;
+            else if (MousePoint.y > TopS)
+                moveDir = Vector3.forward;
+            else if (MousePoint.y < BottomS)
+                moveDir = Vector3.back;
+            else
+            {
+                h = Input.GetAxis("Horizontal");
+                v = Input.GetAxis("Vertical");
+
+                moveDir = (Vector3.forward * v) + (Vector3.right * h);
+            }
+            transform.Translate(moveDir * Time.deltaTime * 20, Space.Self);
+        }
+        else
+        {
+            transform.position = new Vector3(Target.position.x, 10, Target.position.z -6);
+        }
+    }
+    
+    
+}
---
+++ Unity/League of legend reverse develop/Assets/2. Scripts/Hero/HeroController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroController : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public float rotateSpeed = 10.0f;
+
+
+    private Vector3 movePos = Vector3.zero;
+    private Vector3 moveDir = Vector3.zero;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+
+            // 카메라에서 광선을 마우스 클릭된 곳에 조사한다. 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // 조사 지점에 충돌하는 물체가 있는지 판별한다.   
+            if (Physics.Raycast(ray, out RaycastHit raycastHit))
+            {
+                movePos = raycastHit.point;
+                moveDir = movePos - transform.position;
+            }
+        }
+
+        // 보는 방향과 목표 방향을 이용해 회전하고자하는 방향을 구한다.  
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, moveDir, rotateSpeed * Time.deltaTime, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(newDir);
+        transform.position = Vector3.MoveTowards(transform.position, movePos, moveSpeed * Time.deltaTime);
+    }
+
+}
+
+
