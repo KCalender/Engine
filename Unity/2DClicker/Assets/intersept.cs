bc0599b436d4b136a6e88e889c1bc3db217521f5
Calender <pdt0616@gmail.com>
2021-05-10 23:42:50 +0900

    2D 클리커 프로젝트

---
+++ Unity/2DClicker/Assets/intersept.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class intersept : MonoBehaviour, IPointerUpHandler
+{
+    public Scrollbar scrollbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log(this.name);
+        float size = scrollbar.value;
+        if (size > 0.5f)
+        {
+            scrollbar.value = Mathf.Lerp(scrollbar.value, 1.0f, 1f);
+        }
+        else if (size < 0.5f)
+        {
+            scrollbar.value = Mathf.Lerp(scrollbar.value, 0.0f, 1f);
+        }
+    }
+}
---
+++ Unity/2DClicker/readme.md
@@ -0,0 +1,12 @@
+# 2D Clicker Game
+
+[개발 포인트](https://github.com/KCalender/Engine/blob/main/Unity/2DClicker/Dev%20journal/Develop%20Point.md)
+
+[개발 시작 일지](https://github.com/KCalender/Engine/blob/main/Unity/2DClicker/Dev%20journal/2021_05_10.md)
+
+[최근 개발 일지](https://github.com/KCalender/Engine/blob/main/Unity/2DClicker/Dev%20journal/2021_05_10.md)
+
+[개발 완료 유튜브 링크(아직 없음)]()
+
+
+### 참고사이트 및 동영상
